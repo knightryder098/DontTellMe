3fac04503a095c6890e609d500e08b118c40f6a7
knightryder098 <gobinknightryder098@gmail.com>
2022-12-29 13:20:59 +0530

    correct the required urls

--- frontend/src/services/appApi.js
+++ frontend/src/services/appApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const appApi = createApi({
   reducerPath: "appApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: `${process.env.REACT_APP_BACKEND_URL_PROD}`,
   }),
   endpoints: (builder) => ({
     //signup
