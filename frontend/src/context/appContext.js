d8dbd463da2fbe4770c14888cc9b4eec948916d5
knightryder098 <gobinknightryder098@gmail.com>
2022-12-29 12:46:52 +0530

    bug fixed

--- frontend/src/context/appContext.js
+++ frontend/src/context/appContext.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import React from "react";
 
-const SOCKET_URL = `${process.env.REACT_APP_BACKEND_URL}`;
+const SOCKET_URL = `${process.env.REACT_APP_BACKEND_URL_PROD}`;
 
 
 export const socket=io(SOCKET_URL)
